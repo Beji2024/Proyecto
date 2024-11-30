2b9c06db2a1b79086aaac3a1baafcb19c2e4e7f1
Felipe Mosquera <felipemosquera364@gmail.com>
2024-11-30 17:12:55 -0500

    Subo componentes de usuario

--- Trimestre 5/Angular/src/app/app.component.ts
+++ Trimestre 5/Angular/src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./header/header.component";
+import { ContentComponent } from "./content/content.component";
+import { UsuarioComponent } from "../usuario/usuario.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [HeaderComponent, ContentComponent, UsuarioComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })

84fcd26a1457475952dad1d9101562b85ad21313
Felipe Mosquera <felipemosquera364@gmail.com>
2024-11-30 18:54:06 -0500

    Subo componente de la parte de devolucion

--- Trimestre 5/Angular/src/app/app.component.ts
+++ Trimestre 5/Angular/src/app/app.component.ts
@@ -5,10 +5,11 @@ import { ContentComponent } from "./content/content.component";
 import { UsuarioComponent } from "./usuario/usuario.component";
 import { VentaRComponent } from "./venta-r/venta-r.component";
 import { ConsultaVComponent } from "./consulta-v/consulta-v.component";
+import { DevolucionesComponent } from "./devoluciones/devoluciones.component";
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent, ContentComponent, UsuarioComponent, VentaRComponent, ConsultaVComponent],
+  imports: [HeaderComponent, ContentComponent, UsuarioComponent, VentaRComponent, ConsultaVComponent, DevolucionesComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })

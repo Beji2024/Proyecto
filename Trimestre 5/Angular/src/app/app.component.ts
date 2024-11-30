ca603d6a3a084d6085863efe3e03bbaa80d6b46b
Felipe Mosquera <felipemosquera364@gmail.com>
2024-11-30 17:50:27 -0500

    Subo archivo de componente de registro de venta

--- Trimestre 5/Angular/src/app/app.component.ts
+++ Trimestre 5/Angular/src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./header/header.component";
 import { ContentComponent } from "./content/content.component";
-import { UsuarioComponent } from "../usuario/usuario.component";
+import { UsuarioComponent } from "./usuario/usuario.component";
+import { VentaRComponent } from "./venta-r/venta-r.component";
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent, ContentComponent, UsuarioComponent],
+  imports: [HeaderComponent, ContentComponent, UsuarioComponent, VentaRComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ Trimestre 5/Angular/src/app/venta-r/venta-r.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-venta-r',
+  imports: [],
+  templateUrl: './venta-r.component.html',
+  styleUrl: './venta-r.component.css'
+})
+export class VentaRComponent {
+
+}

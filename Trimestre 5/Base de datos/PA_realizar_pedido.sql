cf23cfab1696f7975026f025ddbd87f384285b1a
Breiner27 <breiner-rojas15@hotmail.com>
2024-11-30 22:20:16 -0500

    Add files via upload
    actualizacio al proceso de almacenado / ahora manda un mensaje cuando el id del proveedor no existe

--- Trimestre 5/Base de datos/PA_realizar_pedido.sql
+++ Trimestre 5/Base de datos/PA_realizar_pedido.sql
@@ -1,4 +1,3 @@
-DELIMITER $$
 CREATE DEFINER=`root`@`localhost` PROCEDURE `PA_realizar_pedido`(
     IN fec_ped DATE,        
     IN ped_prov INT,       
@@ -11,7 +10,15 @@ BEGIN
     DECLARE cant INT;
     DECLARE iterador INT DEFAULT 0;
     DECLARE num_productos INT;
-
+    DECLARE proveedor_existe INT;
+    
+    SELECT COUNT(*) INTO proveedor_existe
+    FROM proveedores
+    WHERE nit = ped_prov;
+    
+    IF proveedor_existe = 0 THEN
+		SELECT '¡Atención! El proveedor con el ID proporcionado no existe' AS mensaje_alerta;
+    ELSE
     INSERT INTO pedido (fec_ped, ped_prov, ped_user, ped_est)
     VALUES (fec_ped, ped_prov, ped_user, 1); 
 
@@ -19,12 +26,14 @@ BEGIN
     SET num_productos = JSON_LENGTH(productos);
 
     WHILE iterador < num_productos DO
+
         SET idpro = JSON_UNQUOTE(JSON_EXTRACT(productos, CONCAT('$[', iterador, '].id_pro')));
         SET cant = JSON_UNQUOTE(JSON_EXTRACT(productos, CONCAT('$[', iterador, '].cantidad')));
-        INSERT INTO detalle_ped (cant, det_p_mer, det_p_ped, precio_uni, precio_tt)
-        SELECT cantidad, id_pro, idpedido, m.precio_compra, m.precio_compra * cant
+
+        INSERT INTO detalle_ped (cantidad, det_p_mer, det_p_ped, precio_uni, precio_tt)
+        SELECT cant, id_pro, idpedido, m.precio_compra, m.precio_compra * cant
         FROM mercancia m WHERE m.id_pro = idpro;
         SET iterador = iterador + 1;
     END WHILE;
-END$$
-DELIMITER ;
+    END IF;
+END

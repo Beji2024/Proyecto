a78ddf54eca7fc132453dfa234da08133fe58846
Felipe10041999 <felipemosquera364@gmail.com>
2025-05-10 14:18:38 -0500

    Docs: Creacion carpetas Frontend y Backend

--- Trimestre 7/Beji_Angular/src/app/modelos/categoria.ts
+++
@@ -1,6 +0,0 @@
-export interface Categoria {
-  id?: number;  
-  name: string;
-}
-
-  
--- Trimestre 7/Beji_Angular/src/app/modelos/subcategoria.ts
+++
@@ -1,8 +0,0 @@
-import { Categoria } from './categoria';
-
-export interface Subcategoria {
-  id_sub: number;
-  nom_sub: string;
-  cat_sub: number;
-  categoria?: Categoria; // 🔹 Agrega esta línea para que Angular lo reconozca
-}

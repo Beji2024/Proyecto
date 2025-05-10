f386505bd0b57e6e4407efb5fee02711232168a4
Felipe10041999 <felipemosquera364@gmail.com>
2025-05-10 14:24:15 -0500

    Docs: creacion de carpetas Frontend y Backend

---
+++ Frontend/src/app/app.config.ts
@@ -0,0 +1,19 @@
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import { provideRouter } from '@angular/router';
+import { routes } from './app.routes'; // Asegúrate de que esta ruta esté bien
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { AuthInterceptor} from './interceptores/auth-interceptor.service'; // Importa el interceptor
+
+export const providers = [
+  {
+    provide: HTTP_INTERCEPTORS,
+    useClass: AuthInterceptor,
+    multi: true
+  }
+];
+
+export const appConfig = {
+  providers: [
+    provideRouter(routes),
+  ]
+}

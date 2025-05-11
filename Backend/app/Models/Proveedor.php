a0d74b333455b9cea2610f522c2c6b14d7572c07
breiner <breiner-rojas15@hotmail.com>
2025-05-11 12:17:18 -0500

    Docs: agrego el modelo de proveedor

--- Backend/app/Models/Proveedor.php
+++ Backend/app/Models/Proveedor.php
@@ -2,17 +2,22 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\Hasfactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Proveedor extends Model
 {
-  use HasFactory;
-  protected $fillable = [
-    'nit',
-    'name',
-    'direccion',
-    'telefono',
-    'email'
-  ];
+    use HasFactory;
+    
+    protected $table= 'proveedor';
+    protected $fillable=[
+        'nombre',
+        'nit',
+        'celular',
+        'email',
+        'direccion',
+        'producto',
+        'marca',
+        'valor_unitario'
+    ];
 }

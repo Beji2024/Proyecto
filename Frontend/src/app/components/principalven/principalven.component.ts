08fbb431406d5c7289486dece83f360354a9ef93
Felipe10041999 <felipemosquera364@gmail.com>
2025-06-19 13:04:41 -0500

    feat: Creo el header y sus respectivos direccionamientos de los usuarios vendedor y bodegureo

---
+++ Frontend/src/app/components/principalven/principalven.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { HeadervenComponent } from './headerven/headerven.component';
+
+@Component({
+  selector: 'app-principalven',
+  imports: [HeadervenComponent],
+  templateUrl: './principalven.component.html',
+  styleUrl: './principalven.component.css'
+})
+export class PrincipalvenComponent {
+
+}

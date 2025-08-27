592a5ab28bd78f309d0bd0ca169104bd01cc65f1
Felipe10041999 <felipemosquera364@gmail.com>
2025-08-27 11:14:08 -0500

    Feat: Se crean las apis de proveedores donde se permite en el modulo: Consultar,registrar,actualizar,eliminar,

--- Trimestre 8/BEJI/src/main/kotlin/com/example/BEJI/proveedors/ProveedorsController.kt
+++ Trimestre 8/BEJI/src/main/kotlin/com/example/BEJI/proveedors/ProveedorsController.kt
@@ -1,4 +1,45 @@
 package com.example.BEJI.proveedors
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.web.bind.annotation.*
+import org.springframework.web.bind.annotation.RestController
 
-class proveedorsController {
+@RestController
+class ProveedorsController {
+    @Autowired
+    lateinit var proveedorsService: ProveedorsService
+
+    @GetMapping("/proveedores")
+    fun obtenerProveedors():List<Array<String>>{
+        return proveedorsService.obtenerproveedors()
+    }
+    @PostMapping("/registrar")
+    fun registrarProveedors(@RequestBody proveedors: Proveedors): String{
+        var resultado = proveedorsService.registrarProveedors(proveedors)
+        if(resultado>0){
+            return "Usuario registrado sin problemas"
+        }
+        else{
+            return "Ocurrio un problema, usuario no registado"
+        }
+    }
+    @PutMapping("/actualizar/{id}")
+    fun actualizarProveedors(@PathVariable id:Int,@RequestBody proveedors: Proveedors):String{
+        var resultado = proveedorsService.actualizarProveedors(id,proveedors)
+        if(resultado>0){
+            return "Usuario actualizado con exito"
+        }else{
+            return "Usuario no encontrado"
+        }
+
+    }
+    @DeleteMapping("/eliminar/{id}")
+    fun eliminarProveedors(@PathVariable id:Int): String{
+        var resultado = proveedorsService.eliminarProveedors(id)
+        if(resultado>0){
+            return "Usuario eliminado correctamente"
+        }
+        else{
+            return "Usuario no encontrado"
+        }
+    }
 }
--- Trimestre 8/BEJI/src/main/kotlin/com/example/BEJI/proveedors/ProveedorsService.kt
+++ Trimestre 8/BEJI/src/main/kotlin/com/example/BEJI/proveedors/ProveedorsService.kt
@@ -1,4 +1,46 @@
 package com.example.BEJI.proveedors
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.jdbc.core.JdbcTemplate
+import org.springframework.jdbc.core.RowMapper
+import org.springframework.stereotype.Service
 
+@Service
 class ProveedorsService {
+    @Autowired
+    lateinit var jdbcTemplate: JdbcTemplate
+
+
+    fun obtenerproveedors():List<Array<String>>{
+        val sql = "SELECT * FROM proveedors"
+        return jdbcTemplate.query(sql){rs, _->arrayOf(
+            rs.getString("name"),
+            rs.getString("direccion"),
+            rs.getString("nit"),
+            rs.getString("telefono"),
+            rs.getString("email")
+        )}
+    }
+    fun registrarProveedors(proveedors: Proveedors): Int{
+        val sql = "INSERT INTO proveedors(nit,name,direccion,telefono,email,created_at)VALUES(?,?,?,?,?,NOW())"
+        return jdbcTemplate.update(sql,
+            proveedors.getNit(),
+            proveedors.getName(),
+            proveedors.getDireccion(),
+            proveedors.getTelefono(),
+            proveedors.getEmail())
+    }
+    fun actualizarProveedors(id:Int, proveedors: Proveedors): Int{
+        val sql = "UPDATE proveedors SET nit=?,name=?,direccion=?,telefono=?,email=?,updated_at=NOW() WHERE id =?"
+        return jdbcTemplate.update(sql,
+            proveedors.getNit(),
+            proveedors.getName(),
+            proveedors.getDireccion(),
+            proveedors.getTelefono(),
+            proveedors.getEmail(),id)
+    }
+    fun eliminarProveedors(id:Int): Int{
+        val sql = "Delete from proveedors where id=?"
+        return jdbcTemplate.update(sql,id)
+    }
+
 }

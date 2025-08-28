a77e5304741cc0ad2d31d7136069100da2c651cc
mateogarcia2909 <168492360+mateogarcia2909@users.noreply.github.com>
2025-08-28 10:57:53 -0500

    Api rest ventas SCV

---
+++ src/main/kotlin/com/example/BEJI/proveedors/Proveedors.kt
@@ -0,0 +1,25 @@
+package com.example.BEJI.proveedors
+import org.springframework.format.annotation.DateTimeFormat
+
+data class Proveedors(
+    private var nit :String,
+    private var name:String,
+    private var direccion: String,
+    private var telefono: String,
+    private var email: String,
+){
+    //getters
+    fun getNit(): String{return nit}
+    fun getName(): String{return this.name}
+    fun getDireccion(): String{return this.direccion}
+    fun getTelefono(): String{return this.telefono}
+    fun getEmail(): String{return this.email}
+
+    //setters
+    fun setNit(nit: String){ this.nit = nit }
+    fun setName(name:String){ this.name = name}
+    fun setDireccion(direccion: String){ this.direccion = direccion}
+    fun setTelefono(telefono: String){ this.telefono = telefono}
+    fun setEmail(email: String){ this.email = email}
+
+}

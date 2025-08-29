bd75ee76631fa13c765a122038b25e2eb2031ab1
juan <juanmzpa.2006@gmail.com>
2025-08-28 20:55:43 -0500

    feat: subcategorias API

--- Trimestre 8/BEJI/src/main/kotlin/com/example/BEJI/subcategoria/SubcategoriasController.kt
+++ Trimestre 8/BEJI/src/main/kotlin/com/example/BEJI/subcategoria/SubcategoriasController.kt
@@ -1,4 +1,40 @@
 package com.example.BEJI.subcategoria
-
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.web.bind.annotation.*
+import org.springframework.web.bind.annotation.RestController
+@RestController
 class SubcategoriasController {
+    @Autowired
+    lateinit var subcategoriasServise: SubcategoriasServise
+    @GetMapping("/subcategorias")
+    fun obtenerSubcategorias(): List<Map<String, Any>>{
+        return subcategoriasServise.obtenerSubcategorias()
+    }
+    @PostMapping("/subcategorias")
+    fun crearSubcategoria(@RequestBody subcategorias: Subcategorias): String{
+        var resultado = subcategoriasServise.crearSubcategoria(subcategorias)
+        if(resultado>0){
+            return "Subcategoria creada con exito"
+        }else{
+            return "Ups, no se pudo crear la subcategoria"
+        }
+    }
+    @PutMapping("/subcategorias/{id}")
+    fun actualizarSubcategoria(@PathVariable id: Int,@RequestBody subcategorias: Subcategorias): String{
+        var resultado = subcategoriasServise.actualizarSubcategoria(id,subcategorias)
+        if(resultado>0){
+            return "Subcategoria se a acatualizado correctamente"
+        }else{
+            return "Ups no se ha podido actualizar la subcategoria"
+        }
+    }
+    @DeleteMapping("/subcategorias/{id}")
+    fun eliminarSubcategoria(@PathVariable id: Int): String{
+        var resultado = subcategoriasServise.eliminarSubcategoria(id)
+        if(resultado>0){
+            return "Subactegoria eliminada con exito"
+        }else{
+            return "Ups, no se pudo eliminar la subcategoria"
+        }
+    }
 }

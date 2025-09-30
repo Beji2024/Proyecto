50d7731db25b099aa2cca1c41337c748144f561a
Felipe10041999 <felipemosquera364@gmail.com>
2025-09-30 10:52:15 -0500

    Feat: Se ajusta la vista del RecyclerView de los Usuarios

--- Trimestre 8/Movil-main/AppBase/app/build.gradle.kts
+++ Trimestre 8/Movil-main/AppBase/app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
 
     defaultConfig {
         applicationId = "com.example.appinterface"
-        minSdk = 24
+        minSdk = 25
         targetSdk = 35
         versionCode = 1
         versionName = "1.0"

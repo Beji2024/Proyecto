c700ec0a7bd66382d7f46a44536b85ea8a702b1c
Felipe10041999 <felipemosquera364@gmail.com>
2025-12-05 13:11:19 -0500

    Fix: ajuste del suite2 (Se corrige la importación)

--- Trimestre 9/BejiAutomatizacion/src/test/java/co/com/Automatizacion/runners/Suite/Suite2.java
+++ Trimestre 9/BejiAutomatizacion/src/test/java/co/com/Automatizacion/runners/Suite/Suite2.java
@@ -1,18 +1,17 @@
 package co.com.Automatizacion.runners.Suite;
 
 
-import co.com.Automatizacion.tasks.Producto.CrearProducto;
-import co.com.Automatizacion.tasks.Producto.EditarProducto;
-import co.com.Automatizacion.tasks.Producto.EliminarProducto;
+import co.com.Automatizacion.runners.Productos.CrearProductoRunner;
+import co.com.Automatizacion.runners.Productos.EditarProductoRunner;
+import co.com.Automatizacion.runners.Productos.EliminarProductoRunner;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
-        CrearProducto.class,
-        EditarProducto.class,
-        EliminarProducto.class
-
+        CrearProductoRunner.class,
+        EditarProductoRunner.class,
+        EliminarProductoRunner.class
 })
 public class Suite2 {
 }
